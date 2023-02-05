993a6f9955efd9d5a4e7f40b6eeee2ebc03e0ce1
Kushan <kushaneranga>
2023-02-05 15:14:51 +0530

    Restore

---
+++ src/ReadMoreComponent.vue
@@ -0,0 +1,116 @@
+<template>
+  <div v-if="longText" class="">
+    <div class="">
+      <p v-if="!readMoreActivated && !isLongText">
+        <span v-html="longText"></span>
+      </p>
+      <p v-if="!readMoreActivated && isLongText">
+        <span v-html="shortText"></span>...
+      </p>
+      <p v-if="readMoreActivated"><span v-html="longText"></span></p>
+    </div>
+    <div class="read_more-link">
+      <div class="">
+        <div v-if="!readMoreActivated && isLongText" @click="activateReadMore">
+          {{ readMore }}
+        </div>
+        <div v-if="readMoreActivated && isLongText" @click="deactivateReadMore">
+          {{ readLess }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReadMore",
+  data() {
+    return {
+      longText: "",
+      readMoreActivated: false,
+      windowWidth: window.innerWidth,
+    };
+  },
+  props: {
+    longTextData: {
+      type: String,
+      default:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tellus elit, interdum ac sapien ut, dapibus fermentum purus. In sit amet finibus ante, eu facilisis nisi. Suspendisse elit orci, facilisis a suscipit lacinia, placerat vel est. Proin molestie semper dolor, eget volutpat massa rutrum vitae. Suspendisse finibus facilisis lectus ac commodo. Integer iaculis, ex et fringilla porta, mi ipsum volutpat nisi, et consectetur urna erat a augue. Praesent ligula mi, blandit ut efficitur eu, blandit a nisi. Integer sed mattis dolor, ut efficitur justo. Curabitur mollis quam eros, ut tempus felis gravida ut. In posuere facilisis odio, sed interdum est gravida sollicitudin. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus augue sapien, malesuada at lacus placerat, lobortis cursus risus. Nulla vitae elementum sapien. Nullam nec nulla ac metus varius scelerisque. Duis non diam quis libero tincidunt sollicitudin.",
+    },
+    readMore: {
+      type: String,
+      default: "Read more",
+    },
+    readLess: {
+      type: String,
+      default: "Read less",
+    },
+  },
+  computed: {
+    shortText: function () {
+      if (this.windowWidth < 480) {
+        let str = this.longText
+          .slice(0, this.mobileSize)
+          .replace(/(<([^>]+)>)/gi, "")
+          .match(/.*?[\\ !?]/g);
+        let nstr = str.join("").slice(0, -1);
+        return nstr ? nstr : this.longText.slice(0, this.mobileSize);
+      }
+      if (this.windowWidth < 992) {
+        let str = this.longText
+          .slice(0, this.tabSize)
+          .replace(/(<([^>]+)>)/gi, "")
+          .match(/.*?[\\ !?]/g);
+        let nstr = str.join("").slice(0, -1);
+        return nstr ? nstr : this.longText.slice(0, this.tabSize);
+      }
+      let str = this.longText
+        .slice(0, this.desktopSize)
+        .replace(/(<([^>]+)>)/gi, "")
+        .match(/.*?[\\ !?]/g);
+      let nstr = str.join("").slice(0, -1);
+      return nstr ? nstr : this.longText.slice(0, this.desktopSize);
+    },
+    isLongText: function () {
+      if (this.windowWidth < 480) {
+        return this.longText.length > this.mobileSize;
+      }
+      if (this.windowWidth < 992) {
+        return this.longText.length > this.tabSize;
+      }
+      return this.longText.length > this.desktopSize;
+    },
+    mobileSize: function () {
+      return ((this.windowWidth - 40) / 3.2).toFixed(0) * 3;
+    },
+    tabSize: function () {
+      return ((this.windowWidth - 40) / 14).toFixed(0) * 3;
+    },
+    desktopSize: function () {
+      return ((this.windowWidth - 120) / 10).toFixed(0) * 3;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+    let longTextData = this.longTextData;
+    this.longText = longTextData;
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  methods: {
+    activateReadMore() {
+      this.readMoreActivated = true;
+    },
+    deactivateReadMore() {
+      this.readMoreActivated = false;
+    },
+    onResize() {
+      this.windowWidth = window.innerWidth;
+    },
+  },
+};
+</script>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import ReadMore from "./ReadMoreComponent.vue";
+
+import "./assets/main.css";
+
+createApp(ReadMore).mount("#read-more-text");
