de504bcc1164378bbc8b7c7e6c28d14391e2d6af
Kushan <kushaneranga>
2023-02-08 22:30:35 +0530

    read more demo

---
+++ src/Main.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <input
+      type="range"
+      :min="min"
+      :max="max"
+      v-model="currentValue"
+      class="myRange"
+      :style="{ width: max + 'px' }"
+    />
+    <p>{{ currentValue }}</p>
+    <div class="widthBox" :style="{ width: currentValue + 'px' }">
+      <ReadMore></ReadMore>
+    </div>
+  </div>
+</template>
+
+<script>
+import ReadMore from "./ReadMoreComponent.vue";
+export default {
+  name: "Main",
+  data() {
+    return {
+      min: 0,
+      max: null,
+      currentValue: 50,
+      widthReduce: null,
+    };
+  },
+  components: {
+    ReadMore,
+  },
+  created() {
+    window.resizeTo(800, 600);
+  },
+  mounted() {
+    this.widthReduce = window.innerWidth * 0.2;
+    this.max = window.innerWidth - this.widthReduce;
+    console.log(this.max);
+  },
+  methods: {
+    windowWidth() {
+      this.max = window.innerWidth - 200;
+    },
+  },
+};
+</script>
+
+<style>
+.myRange {
+  width: 100%;
+}
+.widthBox {
+  display: block;
+  background: rgb(20, 208, 214);
+  height: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
-import ReadMore from "./ReadMoreComponent.vue";
+// import ReadMore from "./ReadMoreComponent.vue";
+import Main from "./Main.vue";
 
-createApp(ReadMore).mount("read-more");
+// createApp(ReadMore).mount("read-more");
+createApp(Main).mount("read-more");
